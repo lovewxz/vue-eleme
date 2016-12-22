17437c4fb5d7631793b53e6880abfe65f3e81192
lovewxz <yaojun@yjundeMac-mini.local>
2016-12-22 14:38:09 +0800

    use better-scroll and add foodlist swiper

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,17 +1,45 @@
 <template>
     <div class="goods">
-        <div class="menu-wrapper">
-          <li class="menu-item" v-for="item in goods">
-            <span class="text border-1px">
-              <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
-            </span>
-          </li>
+        <div class="menu-wrapper" v-el:menu-wrapper>
+          <ul>
+            <li class="menu-item" v-for="item in goods" :class="{current: currentIndex === $index}" @click="selectMenu($index,$event)">
+              <span class="text border-1px">
+                <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
+              </span>
+            </li>
+          </ul>
         </div>
-        <div class="foods-wrapper"></div>
+        <div class="foods-wrapper" v-el:foods-wrapper>
+          <ul>
+            <li class="food-list food-list-hook" v-for="item in goods">
+              <h1 class="title">{{item.name}}</h1>
+              <ul>
+                <li class="food-item border-1px" v-for="food in item.foods">
+                  <div class="icon">
+                    <img :src="food.icon" :alt="food.name" width="57" height="57">
+                  </div>
+                  <div class="content">
+                    <h2 class="name">{{food.name}}</h2>
+                    <p class="desc">{{food.description}}</p>
+                    <div class="extra">
+                      <span>月售{{food.sellCount}}</span>
+                      <span class="rating">好评率{{food.rating}}%</span>
+                    </div>
+                    <div class="price">
+                      <span class="now">¥{{food.price}}</span>
+                      <span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
     </div>
 </template>
 
 <script>
+    import BScroll from 'better-scroll'
     const ERR_OK = 0
     export default {
       props: {
@@ -21,7 +49,9 @@
       },
       data() {
         return {
-          goods: []
+          goods: [],
+          listHeight: [],
+          scrollY: 0
         }
       },
       created() {
@@ -29,10 +59,56 @@
           response = response.body
           if (response.errno === ERR_OK) {
             this.goods = response.data
-            console.log(this.goods)
+            this.$nextTick(() => {
+              this._initScroll()
+              this._caculateHeight()
+            })
           }
         })
         this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+      },
+      computed: {
+        currentIndex() {
+          for (let i = 0; i < this.listHeight.length; i++) {
+            let height1 = this.listHeight[i]
+            let height2 = this.listHeight[i + 1]
+            if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+              return i
+            }
+          }
+          return 0
+        }
+      },
+      methods: {
+        selectMenu(index, event) {
+          if (!event._constructed) {
+            return
+          }
+          let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook')
+          let el = foodList[index]
+          this.foodsScroll.scrollToElement(el, 300)
+        },
+        _initScroll() {
+          this.menuScroll = new BScroll(this.$els.menuWrapper, {
+            click: true
+          })
+          this.foodsScroll = new BScroll(this.$els.foodsWrapper, {
+            probeType: 3
+          })
+          this.foodsScroll.on('scroll', (pos) => {
+            this.scrollY = Math.abs(Math.round(pos.y))
+          })
+        },
+        _caculateHeight() {
+          let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook')
+          let height = 0
+          this.listHeight.push(height)
+          for (let i = 0; i < foodList.length; i++) {
+            let itemHeight = foodList[i].clientHeight
+            height += itemHeight
+            this.listHeight.push(height)
+          }
+        }
       }
     }
 </script>
@@ -56,6 +132,15 @@
         display: table;
         line-height: 14px;
         padding: 0 12px;
+        &.current {
+          position: relative;
+          top: -1px;
+          background: #fff;
+          font-weight: 700;
+          .text {
+            @include border-none();
+          }
+        }
         .icon {
           display: inline-block;
           vertical-align: top;
@@ -92,6 +177,65 @@
     }
     .foods-wrapper {
       flex: 1;
+      .title {
+        padding-left: 14px;
+        height: 26px;
+        line-height: 26px;
+        border-left:2px solid #d9dde1;
+        font-size: 12px;
+        color: rgb(147,153,159);
+        background: #f3f5f7;
+      }
+      .food-item {
+        display: flex;
+        margin: 18px;
+        @include border-1px(rgba(7,17,27,0.1));
+        padding-bottom: 18px;
+        &:last-child {
+          @include border-none();
+          margin-bottom: 0px;
+        }
+        .icon {
+          flex: 0 0 57px;
+          margin-right: 10px;
+        }
+        .content {
+          flex: 1;
+          .name {
+            margin-top: 2px;
+            font-size: 14px;
+            line-height: 14px;
+            color: rgb(7,17,27);
+          }
+          .desc {
+            font-size: 10px;
+            line-height: 12px;
+            color: rgb(147,153,159);
+            margin: 8px 0;
+          }
+          .extra {
+            font-size: 10px;
+            line-height: 10px;
+            color: rgb(147,153,159);
+            &.rating {
+              margin-left: 12px;
+            }
+          }
+          .price {
+            line-height: 24px;
+            font-weight: 700;
+            .now {
+              font-size: 14px;
+              color: rgb(240,20,20);
+            }
+            .old {
+              font-size: 10px;
+              color: rgb(147,153,159);
+              text-decoration: line-through;
+            }
+          }
+        }
+      }
     }
 }
 </style>
